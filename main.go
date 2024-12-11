7ce348dc9ce05af2af03d1ef2596ea7e30da01b2
hekai000 <675974119@qq.com>
2024-12-11 14:49:22 +0800

    hk commit feature)增加第8章代码

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
-	"sync"
+	"time"
 )
 
 func sum(id int) {
@@ -14,15 +14,45 @@ func sum(id int) {
 	fmt.Println(id, x)
 }
 
-func main() {
-	wg := new(sync.WaitGroup)
-	wg.Add(2)
+// func main() {
+// 	wg := new(sync.WaitGroup)
+// 	wg.Add(2)
 
-	for i := 0; i < 2; i++ {
-		go func(id int) {
-			defer wg.Done()
-			sum(id)
-		}(i)
-	}
-	wg.Wait()
+// 	for i := 0; i < 2; i++ {
+// 		go func(id int) {
+// 			defer wg.Done()
+// 			sum(id)
+// 		}(i)
+// 	}
+// 	wg.Wait()
+// }
+
+var now = time.Now()
+
+// func init() {
+// 	fmt.Println("init now:", now)
+// }
+
+func init() {
+	fmt.Println("init:", int(time.Now().Sub(now).Seconds()))
+	w := make(chan bool)
+
+	go func() {
+		time.Sleep(time.Second * 3)
+		w <- true
+	}()
+	<-w
 }
+
+func main() {
+	fmt.Println("main:", int(time.Now().Sub(now).Seconds()))
+}
+
+// func main() {
+// 	http.HandleFunc("/qyuhen/test", handler)
+// 	http.ListenAndServe(":80", nil)
+// }
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, `<meta name="go-import" content="test.com/qyuhen/test git https://github.com/qyuhen/test">`)
+// }
